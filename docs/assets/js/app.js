b952e34f8d00634088f355c800ad2f76ba35dbec
Ryan Delk <ryan@thedelk.com>
2018-11-02 16:48:26 -0500

    [feature] add specific word highlighting (needs work)

--- docs/assets/js/app.js
+++ docs/assets/js/app.js
@@ -9,4 +9,53 @@ $(document).ready(function ()
   var lightboxDescription = GLightbox({
     selector: 'lightbox'
   });
+  
+  
+  // ---------------------------------
+  // TESTING
+  // ---------------------------------
+  const grooperTerms = [
+    "image",
+    "images",
+    "page",
+    "pages",
+    // "batch",
+    "batches",
+    "folder",
+    "folders",
+    // "scan",
+    "scanning",
+    "activity",
+    "activities",
+    "property",
+    "properties",
+    // "Process",
+    "Lexicon",
+    "Batch Process",
+    "Scan Profile",
+    "IP Profile",
+    "Separation Profile",
+    "OCR Profile",
+    "Data Type",
+    "Field Class",
+    "Data Field",
+    "Data Model",
+    "(data model)",
+    "(local resources)",
+    "Content Model",
+    "Data Format"
+  ];
+
+  for (let term of grooperTerms) {
+    $('p, .md-content li').html(function(i, html) {
+      var regexp, replacement;
+
+      // https://stackoverflow.com/questions/7959050/use-jquery-filter-to-select-specific-text-within-div
+      regexp = RegExp(`(${term})`, 'g');
+      replacement = `<span class='grooper-word grooper-word-${term}'>$1</span>`;
+
+      return html.replace(regexp, replacement);
+    });
+  }
+
 });
