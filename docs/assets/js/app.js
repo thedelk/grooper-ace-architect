a935225f9abffd7ae2c7ebe54f2d1706fa96d455
Ryan Delk <ryan@thedelk.com>
2018-11-05 10:10:07 -0600

    [test] temporarily disable syntax highlighting - interferes with lightbox

--- docs/assets/js/app.js
+++ docs/assets/js/app.js
@@ -14,49 +14,49 @@ $(document).ready(function ()
   // ---------------------------------
   // TESTING
   // ---------------------------------
-  const grooperTerms = [
-    "image",
-    "images",
-    "page",
-    "pages",
-    "batch",
-    "batches",
-    "folder",
-    "folders",
-    "scan",
-    "scanning",
-    "activity",
-    "activities",
-    "property",
-    "properties",
-    "Lexicon",
-    "Batch Process",
-    "Scan Profile",
-    "IP Profile",
-    "Separation Profile",
-    "OCR Profile",
-    "Data Type",
-    "Field Class",
-    "Data Field",
-    "Data Model",
-    "(data model)",
-    "(local resources)",
-    "Content Model",
-    "Data Format",
-    "Process"
-  ];
+  // const grooperTerms = [
+  //   "image",
+  //   "images",
+  //   "page",
+  //   "pages",
+  //   "batch",
+  //   "batches",
+  //   "folder",
+  //   "folders",
+  //   "scan",
+  //   "scanning",
+  //   "activity",
+  //   "activities",
+  //   "property",
+  //   "properties",
+  //   "Lexicon",
+  //   "Batch Process",
+  //   "Scan Profile",
+  //   "IP Profile",
+  //   "Separation Profile",
+  //   "OCR Profile",
+  //   "Data Type",
+  //   "Field Class",
+  //   "Data Field",
+  //   "Data Model",
+  //   "(data model)",
+  //   "(local resources)",
+  //   "Content Model",
+  //   "Data Format",
+  //   "Process"
+  // ];
 
-  for (let term of grooperTerms) {
-    $('p, .md-content li').html(function(i, html) {
-      var regexp, replacement;
+  // for (let term of grooperTerms) {
+  //   $('p, .md-content li').html(function(i, html) {
+  //     var regexp, replacement;
 
-      // https://stackoverflow.com/questions/7959050/use-jquery-filter-to-select-specific-text-within-div
-      regexp = RegExp(`(${term})`, 'g');
-      termClass = term.replace(" ", "-");
-      replacement = `<span class='grooper-word grooper-word-${termClass}'>$1</span>`;
+  //     // https://stackoverflow.com/questions/7959050/use-jquery-filter-to-select-specific-text-within-div
+  //     regexp = RegExp(`(${term})`, 'g');
+  //     termClass = term.replace(" ", "-");
+  //     replacement = `<span class='grooper-word grooper-word-${termClass}'>$1</span>`;
 
-      return html.replace(regexp, replacement);
-    });
-  }
+  //     return html.replace(regexp, replacement);
+  //   });
+  // }
 
 });
