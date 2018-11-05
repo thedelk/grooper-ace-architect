63dda5602db9513ae4508163bc46fc820e5caf88
Ryan Delk <ryan@thedelk.com>
2018-11-05 14:41:07 -0600

    [test] disable syntax highlighting

--- docs/assets/js/app.js
+++ docs/assets/js/app.js
@@ -14,51 +14,51 @@ $(document).ready(function ()
   // ---------------------------------
   // TESTING
   // ---------------------------------
-  const grooperTerms = [
-    // "image",
-    // "images",
-    // "page",
-    // "pages",
-    "batch",
-    // "batches",
-    // "folder",
-    // "folders",
-    // "scan",
-    // "scanning",
-    // "activity",
-    // "activities",
-    // "process",
-    // "property",
-    // "properties",
-    "Lexicon",
-    "Batch Process",
-    "Scan Profile",
-    "IP Profile",
-    "Separation Profile",
-    "OCR Profile",
-    "Data Type",
-    "Field Class",
-    "Data Field",
-    "Data Model",
-    "(data model)",
-    "(local resources)",
-    "Content Model",
-    "Data Format"
-  ];
+  // const grooperTerms = [
+  //   // "image",
+  //   // "images",
+  //   // "page",
+  //   // "pages",
+  //   "batch",
+  //   // "batches",
+  //   // "folder",
+  //   // "folders",
+  //   // "scan",
+  //   // "scanning",
+  //   // "activity",
+  //   // "activities",
+  //   // "process",
+  //   // "property",
+  //   // "properties",
+  //   "Lexicon",
+  //   "Batch Process",
+  //   "Scan Profile",
+  //   "IP Profile",
+  //   "Separation Profile",
+  //   "OCR Profile",
+  //   "Data Type",
+  //   "Field Class",
+  //   "Data Field",
+  //   "Data Model",
+  //   "(data model)",
+  //   "(local resources)",
+  //   "Content Model",
+  //   "Data Format"
+  // ];
 
-  for (let term of grooperTerms) {
-    $('p:not(.admonition-title), .md-content li').html(function(i, html) {
-    // $('p:not(.admonition-title), .md-content li').html(function(i, html) {
-    // $('p, .md-content li').html(function(i, html) {
-      var regex1, replacement;
+  // for (let term of grooperTerms) {
+  //   $('p:not(.admonition-title), .md-content li').html(function(i, html) {
+  //   // $('p:not(.admonition-title), .md-content li').html(function(i, html) {
+  //   // $('p, .md-content li').html(function(i, html) {
+  //     var regex1, replacement;
 
-      // https://stackoverflow.com/questions/7959050/use-jquery-filter-to-select-specific-text-within-div
-      regex1 = new RegExp('(?<gname>' + term + ')', 'g');
-      termClass = term.replace(" ", "-");
-      replacement = `<span class='grooper-word grooper-word-${termClass}'>$<gname></span>`;
+  //     // https://stackoverflow.com/questions/7959050/use-jquery-filter-to-select-specific-text-within-div
+  //     regex1 = new RegExp('(?<gname>' + term + ')', 'g');
+  //     termClass = term.replace(" ", "-");
+  //     replacement = `<span class='grooper-word grooper-word-${termClass}'>$<gname></span>`;
 
-      return html.replace(regex1, replacement);
-    });
-  }
+  //     return html.replace(regex1, replacement);
+  //   });
+  // }
 
 });
