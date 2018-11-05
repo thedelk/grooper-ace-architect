c8df74e4ae648f9aa3c9dcb0901254f6ac5ff58a
Ryan Delk <ryan@thedelk.com>
2018-11-05 10:10:37 -0600

    Deployed a935225 with MkDocs version: 1.0.4

--- assets/js/app.js
+++ assets/js/app.js
@@ -2,30 +2,61 @@ $(document).ready(function ()
 {
   $('p > img').each(function ()
   {
-    // Add class to images
-    // $(this).addClass('glightbox');
-    
-    
-    
-    // TESTING
-    /////////////////////////////////////////
-    
-    $(this).wrap(`<a class="glightbox5" href="${$(this).prop('src')}"></a>`)
-    
-    
-    
-    
-    
-    
-    /////////////////////////////////////////
-    // END TESTING
-    
-    
-    
+    $(this).wrap(`<a class="lightbox" href="${$(this).prop('src')}"></a>`);
   });
   
   var lightbox = GLightbox();
   var lightboxDescription = GLightbox({
-    selector: 'glightbox5'
+    selector: 'lightbox'
   });
+  
+  
+  // ---------------------------------
+  // TESTING
+  // ---------------------------------
+  // const grooperTerms = [
+  //   "image",
+  //   "images",
+  //   "page",
+  //   "pages",
+  //   "batch",
+  //   "batches",
+  //   "folder",
+  //   "folders",
+  //   "scan",
+  //   "scanning",
+  //   "activity",
+  //   "activities",
+  //   "property",
+  //   "properties",
+  //   "Lexicon",
+  //   "Batch Process",
+  //   "Scan Profile",
+  //   "IP Profile",
+  //   "Separation Profile",
+  //   "OCR Profile",
+  //   "Data Type",
+  //   "Field Class",
+  //   "Data Field",
+  //   "Data Model",
+  //   "(data model)",
+  //   "(local resources)",
+  //   "Content Model",
+  //   "Data Format",
+  //   "Process"
+  // ];
+
+  // for (let term of grooperTerms) {
+  //   $('p, .md-content li').html(function(i, html) {
+  //     var regexp, replacement;
+
+  //     // https://stackoverflow.com/questions/7959050/use-jquery-filter-to-select-specific-text-within-div
+  //     regexp = RegExp(`(${term})`, 'g');
+  //     termClass = term.replace(" ", "-");
+  //     replacement = `<span class='grooper-word grooper-word-${termClass}'>$1</span>`;
+
+  //     return html.replace(regexp, replacement);
+  //   });
+  // }
+
 });
